75a969cd651da62b95454700c0ff4b9ef6104662
James Shi <jshi@live.ca>
2017-10-15 13:34:48 -0700

    added webpack basic build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
-	}
+	},
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React App'
+    })
+  ]
 };
